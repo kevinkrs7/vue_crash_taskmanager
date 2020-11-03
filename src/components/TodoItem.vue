3c5f93a65cf82b4327c9863cb27834f49b45ebea
ubptj <kevin.kraus@student.kit.edu>
2020-11-03 19:19:50 +0100

    little changes

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,18 +1,27 @@
 <template>
-  <div class="todo-item">
-      <p>{{todo.title}}</p>
+  <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+    <p>
+      <input type="checkbox" v-on:change="markComplete" v-bind:checked="todo.completed">
+      {{todo.title}}
+      <button @click="$emit('del-todo', todo.id)" class="del">x</button>
+      </p>
   </div>
 </template>
 
 <script>
 export default {
   name: "TodoItem",
-  props: ["todo"]
+  props: ["todo"],
+  methods: {
+    markComplete() {
+      this.todo.completed = !this.todo.completed;
+    }
+  }
 }
 </script>
 
-<style scoped> 
-.todo-item {
+<style scoped>
+  .todo-item {
     background: #f4f4f4;
     padding: 10px;
     border-bottom: 1px #ccc dotted;
@@ -29,5 +38,4 @@ export default {
     cursor: pointer;
     float: right;
   }
-
 </style>
